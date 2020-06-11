01fc73013898f727802ccb1e6ab68798b7ca0ad0
1155107756 <1483486410zy@gmail.com>
2020-06-11 22:18:56 +0800

    Database Update and fix, new test model and reader fix

--- bricks_modeling/file_IO/model_reader.py
+++ bricks_modeling/file_IO/model_reader.py
@@ -1,8 +1,24 @@
+import math
+
 import numpy as np
 
 from bricks_modeling.bricks.brick_factory import get_all_brick_templates
 from bricks_modeling.bricks.brickinstance import BrickInstance
+from bricks_modeling.file_IO import model_writer
+from util.debugger import MyDebugger
 
+
+def rot_matrix_from_A_to_B(A, B):
+    cross = np.cross(A, B)
+    dot = np.dot(A, B)
+    angle = math.acos(dot)
+    rotation_axes = cross / np.linalg.norm(cross)
+    M = np.array([[0, -rotation_axes[2], rotation_axes[1]],
+                  [rotation_axes[2], 0, -rotation_axes[0]],
+                  [-rotation_axes[1], rotation_axes[0], 0]])
+    if np.linalg.norm(cross) == 0:
+        return np.identity(3, dtype=float)
+    return np.identity(3, dtype=float) + math.sin(angle) * M + (1 - math.cos(angle)) * np.dot(M, M)
 
 def read_bricks_from_file(file_path):
     f = open(file_path, "r")
@@ -86,17 +102,17 @@ def read_bricks_from_file(file_path):
                 # print(f"translate is{trans_matrix[:3, 3:4]}")
                 brickInstance.translate(trans_matrix[:3, 3:4])
 
-                """Following code is for connecting points debugging"""
-                """for cp in brickInstance.get_current_conn_points():
+                '''Following code is for connecting points debugging'''
+                for cp in brickInstance.get_current_conn_points():
                     #print(f"Connecting point position:{cp.pos}")
                     #print(f"Connecting point orientation:{cp.orient}")
 
                     testbrickinstance = BrickInstance(brick_templates[template_ids.index("18654")], np.identity(4, dtype=float), color)
 
                     testbrickinstance.rotate(rot_matrix_from_A_to_B(brick_templates[template_ids.index("18654")].c_points[0].orient, cp.orient))
-                    testbrickinstance.translate(20 * cp.pos)
+                    testbrickinstance.translate(cp.pos)
                     #print(f"rotation matrix is: {testbrickinstance.trans_matrix}")
-                    bricks.append(testbrickinstance)"""
+                    bricks.append(testbrickinstance)
 
                 bricks.append(brickInstance)
                 print(f"brick {brickInstance.template.id} processing done")
@@ -105,3 +121,8 @@ def read_bricks_from_file(file_path):
     f.close()
 
     return bricks
+
+if __name__ == "__main__":
+    debugger = MyDebugger("test")
+    bricks = read_bricks_from_file("../../data/LEGO_models/full_models/gear_Lshape_beam.ldr")
+    model_writer.write_bricks_to_file(bricks, debugger.file_path("model.ldr"))
