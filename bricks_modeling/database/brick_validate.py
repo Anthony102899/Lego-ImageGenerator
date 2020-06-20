d326f8ff1817e8f83788d42b0dd5c2494d091775
haoxu <hao.xu.chn@gmail.com>
2020-06-20 12:02:02 +0800

    minor change

--- bricks_modeling/database/brick_validate.py
+++ bricks_modeling/database/brick_validate.py
@@ -9,7 +9,7 @@ import numpy as np
 
 if __name__ == "__main__":
     debugger = MyDebugger("test")
-    brick_templates, template_ids = get_all_brick_templates(brick_database = ["regular_brick_database_2.json"])
+    brick_templates, template_ids = get_all_brick_templates()
 
     for template in brick_templates:
         brickInstance = BrickInstance(template, np.identity(4, dtype=float), 15)
