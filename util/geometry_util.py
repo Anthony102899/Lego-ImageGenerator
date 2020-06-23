09c2d0c866ebf2d640e6d5a1713fe1d16967b93c
haoxu <hao.xu.chn@gmail.com>
2020-06-23 15:09:48 +0800

    add perpendicular function

--- util/geometry_util.py
+++ util/geometry_util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from numpy import linalg as LA
 
 # TODO: to add function annotation
 def vec_local2world(rot_mat: np.ndarray, local_vec: np.ndarray):
@@ -12,16 +13,60 @@ def point_local2world(
 ):
     return np.dot(rot_mat, local_point) + translation
 
+
 def rot_matrix_from_vec_a_to_b(a, b):
     cross = np.cross(a, b)
-    if np.linalg.norm(cross) == 0: # parallel
+    if np.linalg.norm(cross) == 0:  # parallel
         return np.identity(3, dtype=float)
     else:
         dot = np.dot(a, b)
         angle = math.acos(dot)
         rotation_axes = cross / np.linalg.norm(cross)
-        M = np.array([[0, -rotation_axes[2], rotation_axes[1]],
-                      [rotation_axes[2], 0, -rotation_axes[0]],
-                      [-rotation_axes[1], rotation_axes[0], 0]])
+        M = np.array(
+            [
+                [0, -rotation_axes[2], rotation_axes[1]],
+                [rotation_axes[2], 0, -rotation_axes[0]],
+                [-rotation_axes[1], rotation_axes[0], 0],
+            ]
+        )
 
-        return np.identity(3, dtype=float) + math.sin(angle) * M + (1 - math.cos(angle)) * np.dot(M, M)
+        return (
+            np.identity(3, dtype=float)
+            + math.sin(angle) * M
+            + (1 - math.cos(angle)) * np.dot(M, M)
+        )
+
+
+def get_perpendicular_vec(vec: np.array) -> np.array:
+    assert LA.norm(vec) > 0
+    perp_vec = None
+    if abs(vec[0]) > 1e-10:
+        perp_vec =  np.array([(-vec[1] - vec[2]) / vec[0], 1, 1])
+    elif abs(vec[1]) > 1e-10:
+        perp_vec =  np.array([1, (-vec[0] - vec[2]) / vec[1], 1])
+    else:
+        perp_vec =  np.array([1, 1, (-vec[0] - vec[1]) / vec[2]])
+
+    return perp_vec/LA.norm(perp_vec)
+
+if __name__ == "__main__":
+    for i in range(10):
+        a = np.random.rand(3)
+        b = get_perpendicular_vec(a)
+        print(a, b)
+        print(a.dot(b))
+
+    a = np.array([1, 0, 0])
+    b = get_perpendicular_vec(a)
+    print(a, b)
+    print(a.dot(b))
+
+    a = np.array([0, 5, 0])
+    b = get_perpendicular_vec(a)
+    print(a, b)
+    print(a.dot(b))
+
+    a = np.array([0, 0, 36])
+    b = get_perpendicular_vec(a)
+    print(a, b)
+    print(a.dot(b))
