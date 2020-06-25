a7c60dc3dd8f68c7f7344d21a448cc7e9546665c
LareinaM <mhsj16lareina@gmail.com>
2020-06-25 15:15:14 +0800

    Revert "change json file, substitute axle and pin"
    This reverts commit 2d88d0bead6273b9355d0cf85d24bb0cef8729fa.

--- bricks_modeling/database/brick_validate.py
+++ bricks_modeling/database/brick_validate.py
@@ -1,18 +1,21 @@
+import os
+import sys
+
+ROOT_DIR = os.path.abspath('/Users/wuyifan/lego-solver')
+sys.path.append(ROOT_DIR)
+
+
 from bricks_modeling.file_IO.model_reader import read_bricks_from_file
 from bricks_modeling.file_IO.model_writer import write_bricks_to_file
 from bricks_modeling.connectivity_graph import ConnectivityGraph
 from util.debugger import MyDebugger
-from util.geometry_util import *
 from bricks_modeling.bricks.brick_factory import get_all_brick_templates
 from bricks_modeling.bricks.brickinstance import BrickInstance
-from bricks_modeling.bricks.bricktemplate import BrickTemplate
-from bricks_modeling.connections.connpoint import CPoint
-from bricks_modeling.connections.connpointtype import ConnPointType
 import numpy as np
 
 
 if __name__ == "__main__":
-    debugger = MyDebugger("brick_validate")
+    debugger = MyDebugger("test")
     brick_templates, template_ids = get_all_brick_templates()
 
     for template in brick_templates:
