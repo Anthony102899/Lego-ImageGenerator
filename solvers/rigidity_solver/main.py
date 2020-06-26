043d672e4bff08841df0f7f1b6da29597f89dc48
haoxu <hao.xu.chn@gmail.com>
2020-06-26 16:08:18 +0800

    implement motion visualization

--- solvers/rigidity_solver/main.py
+++ solvers/rigidity_solver/main.py
@@ -12,25 +12,52 @@ import itertools
 from numpy import linalg as LA
 from numpy.linalg import matrix_rank
 import util.geometry_util as geo_util
-from solvers.rigidity_solver.algo_core import spring_energy_matrix
+from solvers.rigidity_solver.algo_core import spring_energy_matrix, tranform_matrix_fitting
 from solvers.rigidity_solver.internal_structure import structure_sampling
 import solvers.rigidity_solver.visualization as vis
+import copy
 
-
-if __name__ == "__main__":
-    debugger = MyDebugger("test")
-    bricks = read_bricks_from_file("./data/full_models/hinged_L.ldr")
-    write_bricks_to_file(
-        bricks, file_path=debugger.file_path("test_single_brick.ldr"), debug=False
-    )
-    structure_graph = ConnectivityGraph(bricks)
-    points, edges = structure_sampling(structure_graph)
+def simulate_step(structure_graph:ConnectivityGraph, n: int, bricks, step_size = 1):
+    structure_graph.bricks = bricks
+    points, edges, points_on_brick = structure_sampling(structure_graph)
 
     M = spring_energy_matrix(points, edges)
 
-    print("problem dimemsion:", M.shape[0])
-    print("matrix rank:", matrix_rank(M))
-
     C = geo_util.eigen(M, symmetric=True)
 
-    vis.show_graph(points, edges, C[1][1].reshape((-1, 3)))
+    e = C[n]
+    e_val, e_vec = e
+    deformed_bricks = copy.deepcopy(bricks)
+    delta_x = e_vec.reshape(-1, 3)
+    for i in range(len(bricks)):
+        indices_on_brick_i = np.array(points_on_brick[i])
+        points_before = points[indices_on_brick_i]
+        points_after = points_before + step_size * delta_x[indices_on_brick_i]
+        R, T = tranform_matrix_fitting(points_before, points_after)
+
+        deformed_bricks[i].trans_matrix[:3, :3] = R @ deformed_bricks[i].trans_matrix[:3, :3]
+        deformed_bricks[i].trans_matrix[:3, 3] = T + deformed_bricks[i].trans_matrix[:3, 3]
+        deformed_bricks[i].color = 4
+    return deformed_bricks
+
+if __name__ == "__main__":
+    debugger = MyDebugger("test")
+    bricks = read_bricks_from_file("./data/full_models/square.ldr")
+    write_bricks_to_file(
+        bricks, file_path=debugger.file_path("model_loaded.ldr"), debug=False
+    )
+    structure_graph = ConnectivityGraph(bricks)
+
+    total_bricks = bricks
+
+    d_bricks = bricks
+    for i in range(100):
+        d_bricks = simulate_step(structure_graph, n=4, bricks=d_bricks, step_size=1)
+        total_bricks += d_bricks
+
+    write_bricks_to_file(
+        total_bricks, file_path=debugger.file_path(f"simulation.ldr"), debug=False
+    )
+
+
+    # vis.show_graph(points, edges, C[1][1].reshape((-1, 3)))
