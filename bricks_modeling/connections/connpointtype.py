b1bef8c9669045b3f60f81b225978955b3d73c00
LareinaM <mhsj16lareina@gmail.com>
2020-06-26 13:43:48 +0800

    code formatting

--- bricks_modeling/connections/connpointtype.py
+++ bricks_modeling/connections/connpointtype.py
@@ -24,13 +24,13 @@ stringToType = {
 
 # property: (debug brick, orientation, offset of the center, scale, scaling direction)
 typeToBrick = {
-    ConnPointType.HOLE: ("18654.dat", [0, 1, 0], [0,0,0], 1,[0, 1, 0]),
-    ConnPointType.PIN: ("4274.dat", [1, 0, 0], [10,0,0],1,[0, 1, 0]),
-    ConnPointType.AXLE : ("3704.dat", [1,0,0],[0,0,0] ,0.5,[1, 0, 0]),
-    ConnPointType.CROSS_HOLE: ("axle.dat", [0, 1, 0], [0,-10,0],20,[0, 1, 0]),
-    ConnPointType.SOLID: ("99948.dat", [0, 1, 0],[0,0,0],0.2225,[1, 1, 1]),
-    ConnPointType.STUD: ("stud.dat", [0, 1, 0],[0,2,0],1,[0, 1, 0]),
-    ConnPointType.TUBE: ("box5.dat", [0, 1, 0],[0,0.5,0],1,[0, 1, 0]),
+    ConnPointType.HOLE: ("18654.dat", [0, 1, 0], [0, 0, 0], 1, [0, 1, 0]),
+    ConnPointType.PIN: ("4274.dat", [1, 0, 0], [10, 0, 0], 1, [0, 1, 0]),
+    ConnPointType.AXLE : ("3704.dat", [1, 0, 0], [0, 0, 0], 0.5, [1, 0, 0]),
+    ConnPointType.CROSS_HOLE: ("axle.dat", [0, 1, 0], [0, -10, 0], 20, [0, 1, 0]),
+    ConnPointType.SOLID: ("99948.dat", [0, 1, 0], [0, 0, 0], 0.2225, [1, 1, 1]),
+    ConnPointType.STUD: ("stud.dat", [0, 1, 0], [0, 2, 0], 1, [0, 1, 0]),
+    ConnPointType.TUBE: ("box5.dat", [0, 1, 0], [0, 0.5, 0], 1, [0, 1, 0]),
 }
 
 if __name__ == "__main__":
