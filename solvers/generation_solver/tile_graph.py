55de8fd92dcd19e7cce6a7ac6d90e327f8ba5a67
LareinaM <mhsj16lareina@gmail.com>
2020-06-30 13:03:11 +0800

    code formatting

--- solvers/generation_solver/tile_graph.py
+++ solvers/generation_solver/tile_graph.py
@@ -11,10 +11,11 @@ connect_type = [
     {ConnPointType.HOLE, ConnPointType.AXLE},
     {ConnPointType.STUD, ConnPointType.TUBE}
     ]
+    
 rot = np.array([
-    [[1,0,0,0],[0,0,-1,0],[0,1,0,0],[0,0,0,1]],
-    [[0,0,1,0],[0,1,0,0],[-1,0,0,0],[0,0,0,1]],
-    [[0,-1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]]
+    [[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]],
+    [[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 0, 1]],
+    [[0, -1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 ])
 
 def get_rotation(cpoint_align, n):
@@ -30,7 +31,7 @@ def get_rotation(cpoint_align, n):
 def get_matrix(cpoint_base, cpoint_align, base_brick: BrickInstance, i):
     translation = np.identity(4)
     rotation = get_rotation(cpoint_align, i)
-    trans_vec = np.append(cpoint_base.pos,1) - (np.append(cpoint_align.pos,1) @ rotation)
+    trans_vec = np.append(cpoint_base.pos, 1) - (np.append(cpoint_align.pos, 1) @ rotation)
     translation[:,3] = trans_vec
     return translation @ rotation
 
@@ -59,6 +60,7 @@ def get_all_tiles(base_brick: BrickInstance, align_tile: BrickInstance, color: i
         
         for i in range(3):
             trans_mat = get_matrix(cpoint_base, cpoint_align, base_brick, i)
+            #print("transformation =\n",trans_mat)
             new_tile = get_new_tile(align_tile, trans_mat, color)  # brick instance, new tile based on "align_tile"
             result_tiles.append(new_tile)
             align_tags.append(align_tag)
@@ -68,7 +70,7 @@ def get_all_tiles(base_brick: BrickInstance, align_tile: BrickInstance, color: i
 def check_repeatability(elem: BrickInstance, result_tiles: list):
     tile_pos = list(map(lambda brick: list(brick.trans_matrix[:,3][:3]), result_tiles))  # a list of positions (#tiles * 3)
     elem_pos = list(elem.trans_matrix[:,3][:3])  # elem position (1 * 3)
-    print("\nelem pos = ", np.array(elem_pos))
+    #print("\nelem pos = ", np.array(elem_pos))
     if list(elem_pos) not in list(tile_pos):
         return False
     result_tiles_idx = list(filter(lambda brick: (elem_pos == brick.trans_matrix[:,3][:3]).all(), result_tiles))  # filtered list of input bricks
@@ -81,7 +83,7 @@ def check_repeatability(elem: BrickInstance, result_tiles: list):
         cp_info.sort()
         elem_cpoints_info.sort()
         if (np.array(cp_info) == np.array(elem_cpoints_info)).all():  # all cpoints in one input equal elem's
-            print("duplicate brick")
+            #print("duplicate brick!")
             return True
             
     return False
@@ -91,10 +93,10 @@ def form_complete_graph(num_rings: int, base_tile: BrickInstance, tile_set: list
     result_tiles = [base_tile]  # the resulting tiles
     last_ring = [base_tile]  # the tiles in the last ring
     for i in range(0, num_rings):
-        print(f"\ncomputing ring_{i}")
+        print(f"\ncomputing ring {i}")
         last_ring_num = len(last_ring)
         for last_ring_idx in range(last_ring_num):  # iterate over all bricks in the last ring
-            print(f"last ring_{last_ring_idx}")
+            print(f"last ring {last_ring_idx}")
             last_layer_brick = last_ring.pop(0)  # brick instance in previous ring
             for align_tile in tile_set:  # brick instance to be aligned
                 neighbour_tiles = get_all_tiles(base_brick=last_layer_brick, align_tile=align_tile, color=i)  # a list of neighbour bricks of "base_brick"
