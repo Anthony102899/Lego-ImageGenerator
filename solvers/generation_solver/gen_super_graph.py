ec2fb315580e6901431b367e8fd175e388295a4b
LareinaM <mhsj16lareina@gmail.com>
2020-07-11 23:12:41 +0800

    mistake

--- solvers/generation_solver/gen_super_graph.py
+++ solvers/generation_solver/gen_super_graph.py
@@ -1,7 +1,3 @@
-import os
-import sys
-ROOT_DIR = os.path.abspath('/Users/wuyifan/lego-solver')
-sys.path.append(ROOT_DIR)
 from bricks_modeling.file_IO.model_reader import read_bricks_from_file
 from solvers.generation_solver.tile_graph import find_brick_placements
 from util.debugger import MyDebugger
