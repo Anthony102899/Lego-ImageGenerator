612fed3b5b99a05317dbc20778ee3c9e533ff702
xuhaocuhk <hao.xu.chn@gmail.com>
2020-07-22 12:40:25 +0800

    big changes

--- solvers/brick_heads/main.py
+++ solvers/brick_heads/main.py
@@ -1,79 +1,43 @@
-from bricks_modeling.file_IO.model_reader import read_bricks_from_file
 from bricks_modeling.file_IO.model_writer import write_bricks_to_file
 from bricks_modeling.connectivity_graph import ConnectivityGraph
 from util.debugger import MyDebugger
 import os
 import itertools as iter
 import json
-from solvers.brick_heads.part_selection import get_part_files, select_nearest_face_color
+import solvers.brick_heads.part_selection as p_select
 import copy
+import numpy as np
+import csv
+from typing import Tuple,List
+import solvers.brick_heads.bach_render_images as render
 
-parts = ["hair", "glasses", "beard", "clothes",  "hands" ]
+def gen_LEGO_figure(
+    gender: int,
+    hair: int,
+    hair_color: Tuple,
+    bang: int,
+    skin_color: int,
+    eye: int,
+    jaw: int,
+    hands: int,
+    clothes_style: int,
+):
+    # Template
+    template_bricks = p_select.gen_template()
 
-template_path = "./solvers/brick_heads/template.ldr"
-parts_dir = "./solvers/brick_heads/parts/"
-input_dir = f"./solvers/brick_heads/input_images/"
+    # Hair
+    hair_bricks = p_select.gen_hair(gender, hair, hair_color, bang, skin_color)
 
+    # Eye
+    eye_bricks = p_select.gen_eyes(eye, skin_color)
 
-def get_skin_files(selected_files, json_data):
-    skin_files = []
+    # Jaw
+    jaw_bricks = p_select.gen_jaw(jaw, skin_color)
 
-    skin_color = json_data["skin"]
-    color_id = select_nearest_face_color(skin_color)
+    # Hands
+    hands_bricks = p_select.gen_hands(hands, skin_color)
 
-    for file in selected_files:
-        skined_file = file[0] + "_skin"
-        file_path = parts_dir + skined_file + ".ldr"
-        if os.path.exists(file_path):
-            skin_files.append((skined_file, color_id))
-
-    return skin_files
-
-def gen_LEGO_figure(json_data):
-    selected_files = []
-
-    for i in range(len(parts)):
-        part_selection = get_part_files(parts[i], json_data)
-        skin_files = get_skin_files(part_selection, json_data)
-        selected_files += part_selection
-        selected_files += skin_files
-
-    # start reading bricks
-    total_bricks = []
-    template_bricks = read_bricks_from_file(
-        template_path, read_fake_bricks=True
-    )
-    total_bricks += template_bricks
-
-    for file in selected_files:
-        bricks = read_bricks_from_file(parts_dir + file[0] + ".ldr", read_fake_bricks=True)
-        ldraw_color = file[1]
-        if ldraw_color is not None:
-            for b in bricks:
-                b.color = ldraw_color
-        total_bricks += bricks
-
-    return total_bricks
-
-def gen_all_inputs():
-    files = []
-
-    with open(input_dir + "5_Hepburn.json") as f:
-        json_data = json.load(f)
-
-    for gender in [0, 1]:
-        for hair in [1,2,3]:
-            for glasses in [-1, 1]:
-                for beard in [-1,1]:
-                    for bang in [0,1]:
-                        new_json = copy.deepcopy(json_data)
-                        new_json["gender"] = gender
-                        new_json["hair"][1] = hair
-                        new_json["glasses"] = glasses
-                        new_json["beard"] = beard
-                        new_json["hair"][2][1] = bang
-                        files.append((new_json, f"{gender}_{hair}_{glasses}_{beard}_{bang}"))
-    return files
+    return template_bricks + hair_bricks + eye_bricks + jaw_bricks + hands_bricks
 
 def ouptut_all_inputs():
     fake_inputs = gen_all_inputs()
@@ -85,21 +49,34 @@ def ouptut_all_inputs():
             bricks, file_path=debugger.file_path(f"{file[1]}.ldr"), debug=False
         )
 
+
+
 if __name__ == "__main__":
     debugger = MyDebugger("brick_heads")
-    # ouptut_all_inputs()
+    csv_path = r"/Users/apple/Dropbox/deecamp/data.csv"
 
-    files = ["1_lkf", "2_gxs", "3_ymh", "4_taylor", "5_Hepburn", "6_James"]
-    # files = ["6_James"]
+    with open(csv_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        csv_matrix = list(csv_reader)
 
-    for input_figure in files:
-        with open(input_dir + f"{input_figure}.json") as f:
-            json_data = json.load(f)
-
-        bricks = gen_LEGO_figure(json_data)
-
-        write_bricks_to_file(
-            bricks, file_path=debugger.file_path(f"complete_{input_figure}.ldr"), debug=False
+    for model in csv_matrix[2:3]:
+        if model[1] == "":
+            continue
+        bricks = gen_LEGO_figure(
+            gender = int(model[1]),
+            hair = int(model[2]),
+            hair_color = (int(model[3].split(",")[0]), int(model[3].split(",")[1]), int(model[3].split(",")[2])),
+            bang = int(model[4]),
+            skin_color= int(model[5]),
+            eye= int(model[6]),
+            jaw= int(model[7]),
+            hands= int(model[8]),
+            clothes_style= int(model[9]),
         )
 
-
+        write_bricks_to_file(
+            bricks,
+            file_path=debugger.file_path(f"complete_{int(model[0])}.ldr"),
+            debug=False,
+        )
+    render.render_ldrs(debugger._debug_dir_name)
--- solvers/brick_heads/part_selection.py
+++ solvers/brick_heads/part_selection.py
@@ -1,14 +1,19 @@
 from bricks_modeling.database import ldraw_colors
+from typing import List
+import solvers.brick_heads.config as conf
+from bricks_modeling.file_IO.model_reader import read_bricks_from_file
+import os
 
-def select_nearest_face_color(rgb):
+def get_skin_color(skin_color: int):
     skin_color_map = {
-        (244,244,244):511, # white
-        (255,201,149):78, # yellow
-        (145,80,28):484  # black or 10484?
+        2: 511,  # white
+        1: 78,  # yellow
+        0: 484,  # black or 10484?
     }
-    return select_nearest_color(rgb, given_list=skin_color_map)
+    return skin_color_map[skin_color]
 
-def select_nearest_color(rgb, given_list = None):
+
+def nearest_color_id(rgb, given_list=None):
     if given_list is None:
         all_colors = ldraw_colors.read_colors()
     else:
@@ -17,31 +22,95 @@ def select_nearest_color(rgb, given_list = None):
     best_id = -1
     closest_dist = 1e8
     for l_rgb, color_id in all_colors.items():
-        current_dist = (rgb[0]-l_rgb[0])**2+(rgb[1]-l_rgb[1])**2+(rgb[2]-l_rgb[2])**2
+        current_dist = (
+            (rgb[0] - l_rgb[0]) ** 2
+            + (rgb[1] - l_rgb[1]) ** 2
+            + (rgb[2] - l_rgb[2]) ** 2
+        )
         if current_dist < closest_dist:
             best_id = color_id
             closest_dist = current_dist
 
     return best_id
 
+def gen_template():
+    template_file = "template"
+    template_bricks = get_bricks_from_files([template_file])
+    return template_bricks
+
+def gen_hair(gender:int, hair:int, hair_color:str, bang:int, skin_color:int):
+    gender = "M" if gender == 0 else "F"
+    hair_file = f"hair/{gender}-Hair-{hair}"
+    hair_lh_file = f"hair/{gender}-Hair-{hair}_lh" if bang>0 else "hair/Hair_no_lh"
+    hair_files = [hair_file, hair_lh_file] if hair_lh_file is not None else [hair_file]
+    hair_skin_files = get_skin_files(hair_files)
+
+    hair_color_id = nearest_color_id(hair_color)
+    skin_color_id = get_skin_color(skin_color)
+
+    hair_bricks = get_bricks_from_files(hair_files, hair_color_id)
+    hair_skin_bricks = get_bricks_from_files(hair_skin_files, skin_color_id)
+
+    return hair_bricks + hair_skin_bricks
+
+def gen_eyes(eye:int, skin_color:int):
+    eye_file = "eyes/eyes_0" if eye == 0 else "eyes/eyes_glasses"
+    eye_skin_file = get_skin_files([eye_file])
+
+    skin_color_id = get_skin_color(skin_color)
+
+    eye_bricks = get_bricks_from_files([eye_file])
+    eye_skin_bricks = get_bricks_from_files(eye_skin_file, skin_color_id)
+
+    return eye_bricks + eye_skin_bricks
+
+def gen_hands(hands:int, skin_color:int):
+    hands_file = f"hands/hands_down_{hands}"
+    hands_skin_file = get_skin_files([hands_file])
+
+    skin_color_id = get_skin_color(skin_color)
+
+    hands_bricks = get_bricks_from_files([hands_file])
+    hands_skin_bricks = get_bricks_from_files(hands_skin_file, skin_color_id)
+
+    return hands_bricks + hands_skin_bricks
+
+def gen_jaw(jaw, skin_color:int):
+    if jaw == 3: #unsupported jaw
+        jaw = 0
+    jaw_file = f"jaw/jaw_{jaw}"
+    jaw_skin_files = get_skin_files([jaw_file])
+
+    skin_color_id = get_skin_color(skin_color)
+
+    jaw_bricks = get_bricks_from_files([jaw_file])
+    jaw_skin_bricks = get_bricks_from_files(jaw_skin_files, skin_color_id)
+
+    return jaw_bricks + jaw_skin_bricks
+
+
 def get_part_files(body, json_data):
     if body == "hair":
         gender = "F" if json_data["gender"] == 0 else "M"
         length = json_data["hair"][1]
-        nearest_ldr_color = select_nearest_color(rgb=json_data["hair"][0])
+        nearest_ldr_color = nearest_color_id(rgb=json_data["hair"][0])
 
-        if sum(json_data["hair"][2])==0: # no bang
-            return [(f"{gender}-Hair-{length}", nearest_ldr_color),
-                    ("Hair_no_lh", nearest_ldr_color)]
+        if sum(json_data["hair"][2]) == 0:  # no bang
+            return [
+                (f"{gender}-Hair-{length}", nearest_ldr_color),
+                ("Hair_no_lh", nearest_ldr_color),
+            ]
         else:
-            return [(f"{gender}-Hair-{length}",nearest_ldr_color),
-                    (f"{gender}-Hair-{length}_lh", nearest_ldr_color)]
+            return [
+                (f"{gender}-Hair-{length}", nearest_ldr_color),
+                (f"{gender}-Hair-{length}_lh", nearest_ldr_color),
+            ]
 
     elif body == "hands":
         return [("08_hands_front_non", None)]
 
     elif body == "clothes":
-        nearest_ldr_color = select_nearest_color(rgb=json_data["clothes"][0])
+        nearest_ldr_color = nearest_color_id(rgb=json_data["clothes"][0])
         return [("clothes", nearest_ldr_color)]
 
     elif body == "glasses":
@@ -56,3 +125,32 @@ def get_part_files(body, json_data):
     else:
         print("error id:", body_id)
         input()
+
+
+def get_skin_files(selected_files):
+    skin_files = []
+
+    for file in selected_files:
+        skined_file = file + "_skin"
+        file_path = conf.parts_dir + skined_file + ".ldr"
+        if os.path.exists(file_path):
+            skin_files.append(skined_file)
+
+    return skin_files
+
+
+# get the bricks of the indicate files
+def get_bricks_from_files(files: List[str], assign_color_id=None):
+    total_bricks = []
+
+    for file in files:
+        bricks = read_bricks_from_file(
+            conf.parts_dir + file + ".ldr", read_fake_bricks=True
+        )
+        total_bricks += bricks
+
+    if assign_color_id is not None:
+        for b in total_bricks:
+            b.color = assign_color_id
+
+    return total_bricks
