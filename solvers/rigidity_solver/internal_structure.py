c47849f7f6ec3afd9d6b00ec50e79cf37e81bb63
1155107756 <1483486410zy@gmail.com>
2020-08-15 22:07:53 +0800

    Update joint sampling

--- solvers/rigidity_solver/internal_structure.py
+++ solvers/rigidity_solver/internal_structure.py
@@ -56,7 +56,7 @@ def structure_sampling(structure_graph: ConnectivityGraph):
 
         # generate points around the contact points
         p = get_crystal_vertices(contact_pt, contact_orient)
-
+        #share points parallel to the contact_orient
         if edge["type"] == ConnType.HOLE_PIN.name:
             for i in range(7):
                 if i in {0, 1, 2}:
@@ -68,13 +68,13 @@ def structure_sampling(structure_graph: ConnectivityGraph):
                     points_on_brick[bi].append(len(points) - 1)
                     points.append(p[i])
                     points_on_brick[bj].append(len(points) - 1)
-
+        #share all points
         if edge["type"] == ConnType.CROSS_AXLE.name:
             for i in range(7):
                 points.append(p[i])
                 points_on_brick[bi].append(len(points) - 1)
                 points_on_brick[bj].append(len(points) - 1)
-
+        # share points parallel to the contact_orient
         if edge["type"] == ConnType.STUD_TUBE.name:
             for i in range(7):
                 if i in {0, 1, 2}:
@@ -89,24 +89,26 @@ def structure_sampling(structure_graph: ConnectivityGraph):
 
         if edge["type"] == ConnType.HOLE_AXLE.name:
             #they do not share any points
+            points_base_index = len(points)
             for i in range(7):
                 points.append(p[i])
                 points_on_brick[bi].append(len(points) - 1)
                 points.append(p[i])
                 points_on_brick[bj].append(len(points) - 1)
-                edges.append([len(points) - 2, len(points) - 1])
 
             p_vec1, p_vec2 = geo_util.get_perpendicular_vecs(contact_orient)
-            abstract_edges[points_base_index + 1] = p_vec1
-            abstract_edges[points_base_index + 3] = p_vec2
-            abstract_edges[points_base_index + 5] = (p_vec2 + p_vec1)/2
-            abstract_edges[points_base_index + 7] = p_vec1
-            abstract_edges[points_base_index + 9] = -p_vec1
-            abstract_edges[points_base_index + 11] = p_vec2
-            abstract_edges[points_base_index + 13] = -p_vec2
+            p_vec3 = (p_vec1 + p_vec2) / 2
+            abstract_edges.append([points_base_index, points_base_index + 1, p_vec1[0], p_vec1[1], p_vec1[2]])
+            abstract_edges.append([points_base_index + 2,points_base_index + 3, p_vec2[0], p_vec2[1], p_vec2[2]])
+            abstract_edges.append([points_base_index + 4,points_base_index + 5, p_vec3[0], p_vec3[1], p_vec3[2]])
+            abstract_edges.append([points_base_index + 6,points_base_index + 7, p_vec1[0], p_vec1[1], p_vec1[2]])
+            abstract_edges.append([points_base_index + 8,points_base_index + 9, -p_vec1[0], -p_vec1[1], -p_vec1[2]])
+            abstract_edges.append([points_base_index + 10,points_base_index + 11, p_vec2[0], p_vec2[1], p_vec2[2]])
+            abstract_edges.append([points_base_index + 12,points_base_index + 13, -p_vec2[0], -p_vec2[1], -p_vec2[2]])
         #This is a conntype not in our database
 
         if edge["type"] == ConnType.PRISMATIC.name:
+            points_base_index = len(points)
             for i in range(7):
                 points.append(p[i])
                 points_on_brick[bi].append(len(points) - 1)
@@ -115,13 +117,14 @@ def structure_sampling(structure_graph: ConnectivityGraph):
                 edges.append([len(points) - 2, len(points) - 1])
 
             p_vec1, p_vec2 = geo_util.get_perpendicular_vecs(contact_orient)
-            abstract_edges[points_base_index + 1] = p_vec1
-            abstract_edges[points_base_index + 3] = p_vec2
-            abstract_edges[points_base_index + 5] = (p_vec2 + p_vec1)/2
-            abstract_edges[points_base_index + 7] = p_vec2
-            abstract_edges[points_base_index + 9] = -p_vec2
-            abstract_edges[points_base_index + 11] = p_vec1
-            abstract_edges[points_base_index + 13] = -p_vec1
+            p_vec3 = (p_vec1 + p_vec2) / 2
+            abstract_edges.append([points_base_index, points_base_index + 1, p_vec1[0], p_vec1[1], p_vec1[2]])
+            abstract_edges.append([points_base_index + 2, points_base_index + 3, p_vec2[0], p_vec2[1], p_vec2[2]])
+            abstract_edges.append([points_base_index + 4, points_base_index + 5, p_vec3[0], p_vec3[1], p_vec3[2]])
+            abstract_edges.append([points_base_index + 6, points_base_index + 7, p_vec2[0], p_vec2[1], p_vec2[2]])
+            abstract_edges.append([points_base_index + 8, points_base_index + 9, -p_vec2[0], -p_vec2[1], -p_vec2[2]])
+            abstract_edges.append([points_base_index + 10, points_base_index + 11, p_vec1[0], p_vec1[1], p_vec1[2]])
+            abstract_edges.append([points_base_index + 12, points_base_index + 13, -p_vec1[0], -p_vec1[1], -p_vec1[2]])
 
         # remove the generated points
         representative_cpoints_on_brick[bi].discard(edge["cpoint_indices"][0])
