33263e69edc892c28a3c3bd7f111e4c173574f1d
xuhaocuhk <hao.xu.chn@gmail.com>
2020-08-17 15:37:12 +0800

    add more 2D cases

--- solvers/rigidity_solver/main_2d.py
+++ solvers/rigidity_solver/main_2d.py
@@ -12,7 +12,7 @@ import solvers.rigidity_solver.test_cases.cases_2D as cases2d
 if __name__ == "__main__":
     debugger = MyDebugger("test")
 
-    points, fixed_points_index, edges, abstract_edges = cases2d.case_1()
+    points, fixed_points_index, edges, abstract_edges = cases2d.case_6()
 
     is_rigid, eigen_pairs = solve_rigidity(points, edges + abstract_edges, fixed_points=fixed_points_index, dim=2)
 
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         vec, value = get_weakest_displacement(eigen_pairs, dim=2)
         vis.visualize_2D(points, edges, vec)
     else:
-        motion_vecs = get_motions(eigen_pairs, dim=2)
+        motion_vecs = get_motions(eigen_pairs, points, dim=2)
         vis.visualize_2D(points, edges, motion_vecs[0])
 
     print(is_rigid)
--- solvers/rigidity_solver/test_cases/cases_2D.py
+++ solvers/rigidity_solver/test_cases/cases_2D.py
@@ -23,3 +23,27 @@ def case_3():
     edges = [(0, 1), (0, 2)]
     abstract_edges = []
     return points, fixed_points_index, edges, abstract_edges
+
+# a triangle with one point not connected
+def case_4():
+    points = np.array([[0, 0], [1, 0], [0, 1],[0, 1]])
+    fixed_points_index = []
+    edges = [(0, 1), (1, 2), (0, 3)]
+    abstract_edges = []
+    return points, fixed_points_index, edges, abstract_edges
+
+# a triangle with one point not connected and one edge fixed
+def case_5():
+    points = np.array([[0, 0], [1, 0], [0, 1],[0, 1]])
+    fixed_points_index = [0, 1]
+    edges = [(0, 1), (1, 2), (0, 3)]
+    abstract_edges = []
+    return points, fixed_points_index, edges, abstract_edges
+
+# a triangle with one point not connected and one edge fixed. The dangling point can slide over adjacent edge
+def case_6():
+    points = np.array([[0, 0], [1, 0], [0, 1],[0, 1]])
+    fixed_points_index = [0, 1]
+    edges = [(0, 1), (1, 2), (0, 3)]
+    abstract_edges = [(2, 3, 1.0, 0.0)]
+    return points, fixed_points_index, edges, abstract_edges
