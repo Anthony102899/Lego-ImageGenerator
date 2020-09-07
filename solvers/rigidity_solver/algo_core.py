695808ae485570c443be251e8e824b38dc1f05b0
haoxu <hao.xu.chn@gmail.com>
2020-09-07 18:00:13 +0800

    update 2D cases and algorithm

--- solvers/rigidity_solver/algo_core.py
+++ solvers/rigidity_solver/algo_core.py
@@ -12,6 +12,10 @@ import itertools
 from numpy import linalg as LA
 from numpy.linalg import inv, matrix_rank
 from visualization.model_visualizer import visualize_3D, visualize_2D
+from scipy.linalg import null_space
+from numpy.linalg import cholesky
+from numpy.linalg import inv
+from numpy.linalg import matrix_rank
 
 def rigidity_matrix(
     points: np.ndarray,
@@ -149,3 +153,117 @@ def solve_rigidity(points: np.ndarray, edges: np.ndarray, fixed_points = [], dim
         return True, non_zero_eigenspace
     else:
         return False, zero_eigenspace
+
+def solve_rigidity_new(points: np.ndarray, edges: np.ndarray, joints, fixed_points_idx = [], dim: int = 3) -> (bool, List[np.ndarray]):
+    M = spring_energy_matrix(points, edges, dim=dim, fixed_points_idx=[])
+
+    A = np.zeros((1, len(points) * dim))
+    for joint in joints:
+        edge_idx, motion_points, allowed_motions = joint[0], joint[1], joint[2]
+
+        A_joint = constraints_for_one_joint(points[np.array(list(edges[edge_idx]))], list(edges[edge_idx]),
+                                      points[np.array(motion_points)], motion_points,
+                                      points,
+                                      allowed_motions=allowed_motions)
+        A = np.append(A, A_joint, 0)
+
+    # adding the fixed points constraints
+    C = get_matrix_for_fixed_points(fixed_points_idx, len(points), dim)
+    A = np.append(A, C, 0)
+
+    # mathmatical computation
+    B = null_space(A)
+    T = np.transpose(B) @ B
+    L = cholesky(T)
+    S = B.T @ M @ B
+    print("T rank:", matrix_rank(T))
+    print("S rank:", matrix_rank(S))
+
+    # compute eigen value / vectors
+    eigen_pairs = geo_util.eigen(inv(L).T @ S @ inv(L), symmetric=True)
+    eigen_pairs = [(e_val, B @ e_vec) for e_val, e_vec in eigen_pairs]
+
+    # judge rigid or not
+    print("DoF:", len([(e_val, e_vec) for e_val, e_vec in eigen_pairs if abs(e_val) < 1e-6]))
+
+    trivial_motions     = [(e_val, e_vec) for e_val, e_vec in eigen_pairs if
+                           abs(e_val) < 1e-6 and geo_util.is_trivial_motions([e_vec], points, dim)]
+
+    non_trivial_motions = [(e_val, e_vec) for e_val, e_vec in eigen_pairs if
+                           abs(e_val) < 1e-6 and (not geo_util.is_trivial_motions([e_vec], points, dim))]
+
+    non_zero_eigenspace = [(e_val, e_vec) for e_val, e_vec in eigen_pairs if abs(e_val) >= 1e-6]
+
+    return trivial_motions, non_trivial_motions, non_zero_eigenspace
+
+def constraints_for_one_joint(source_pts, source_pts_idx, target_pts, target_pts_idx, points, allowed_motions, dim = 2):
+    points_num = len(points)
+
+    forbidden_motion_vecs = get_constraits_for_points_allowed_motions(allowed_motions, target_pts, dim=dim)
+
+    constraint_mat = np.zeros((len(forbidden_motion_vecs), (len(target_pts) * dim)))
+    project_mat = np.zeros((len(target_pts) * dim, points_num * dim))
+
+    b1_length = LA.norm(source_pts[1] - source_pts[0])
+    basis1 = (source_pts[1] - source_pts[0]) / b1_length
+    basis2 = np.array([-basis1[1], basis1[0]])
+    idx_0, idx_1 = source_pts_idx[0], source_pts_idx[1]
+
+    # assemble projection matrix
+    for i in range(len(target_pts)):
+        jo2_idx = target_pts_idx[i]
+        # assembly the coordinate m
+        project_mat[i * dim, jo2_idx * dim : jo2_idx * dim + dim ]  = np.transpose(basis1)
+        project_mat[i * dim, idx_0 * dim   : idx_0 * dim + dim]     = np.transpose((points[idx_0] - points[jo2_idx])/b1_length - basis1)
+        project_mat[i * dim, idx_1   * dim : idx_1   * dim + dim ]  = np.transpose((points[jo2_idx] - points[idx_0])/b1_length)
+
+        # assembly the coordinate n
+        vec = (points[jo2_idx] - points[idx_0]) / b1_length
+        project_mat[i * dim + 1, jo2_idx * dim : jo2_idx * dim + dim ] = np.transpose(basis2)
+        project_mat[i * dim + 1, idx_0 * dim]     = -vec[1] - basis2[0]
+        project_mat[i * dim + 1, idx_0 * dim + 1] =  vec[0] - basis2[1]
+        project_mat[i * dim + 1, idx_1 * dim]     =  vec[1]
+        project_mat[i * dim + 1, idx_1 * dim + 1] = -vec[0]
+
+    # assemble the constraint matrix via forbidden motion
+    for idx, motion_vec in enumerate(forbidden_motion_vecs):
+        for j in range(len(target_pts)):
+            projected_value_1 = np.transpose(motion_vec[j*dim : j*dim + dim]) @ basis1
+            projected_value_2 = np.transpose(motion_vec[j*dim : j*dim + dim]) @ basis2
+            constraint_mat[idx, j * dim]     = projected_value_1
+            constraint_mat[idx, j * dim + 1] = projected_value_2
+
+    A = constraint_mat @ project_mat
+
+    print(f"constraint matrix: rank{matrix_rank(constraint_mat)}")
+    print(constraint_mat)
+    print(f"projection matrix: rank{matrix_rank(project_mat)}")
+    print(project_mat)
+    print(f"A: rank{matrix_rank(A)}")
+    print(A)
+
+    return A
+
+def get_matrix_for_fixed_points(fixed_points_index, points_num, dim):
+    C = np.zeros((len(fixed_points_index) * dim, points_num * dim))
+    # for fixed joints
+    for row, point_idx in enumerate(fixed_points_index):
+        C[row * dim: row * dim + dim, point_idx * dim: point_idx * dim + dim] = np.identity(dim)
+
+    return C
+
+# get constraint for a single point
+def get_constraits_for_points_allowed_motions(allowed_motions, target_points, dim):
+    allowed_motion_vecs = np.zeros((len(allowed_motions) * len(target_points), len(target_points) * dim))
+
+    for idx, constraint in enumerate(allowed_motions):
+        for j in range(len(target_points)):
+            row_num = idx * len(target_points) + j
+            if constraint[0] == "T":  # translation
+                allowed_motion_vecs[row_num, j * dim: j * dim + 2] = constraint[1]
+            elif constraint[0] == "R":
+                rot_arm = target_points[j] - constraint[1]
+                allowed_motion_vecs[row_num, j * dim] = -rot_arm[1]
+                allowed_motion_vecs[row_num, j * dim + 1] = rot_arm[0]
+
+    return null_space(allowed_motion_vecs).T
--- solvers/rigidity_solver/eigen_analysis.py
+++ solvers/rigidity_solver/eigen_analysis.py
@@ -13,7 +13,7 @@ def get_motions(eigen_pairs, points, dim):
     # cast the eigenvectors corresponding to zero eigenvalues into nullspace of the trivial basis,
     # in other words, the new vectors don't have any components (projection) in the span of the trivial basis
 
-    if geo_util.is_subspace(zeroeigenspace, basis):
+    if geo_util.is_subspace(basis, zeroeigenspace):
         reduced_zeroeigenspace = [geo_util.subtract_orthobasis(vec, basis) for vec in zeroeigenspace]
         motion_basis = geo_util.rref(reduced_zeroeigenspace)
         trivial_motion_dim = 3 if dim == 2 else 6
