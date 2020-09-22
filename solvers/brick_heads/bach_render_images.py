2f49d660a19c09ddd83a74453feedeb36ae8211e
xuhaocuhk <hao.xu.chn@gmail.com>
2020-09-22 22:00:47 +0800

    update code for brick headz

--- solvers/brick_heads/bach_render_images.py
+++ solvers/brick_heads/bach_render_images.py
@@ -15,10 +15,10 @@ def render_ldrs(folder_path):
         # os.system(f"{ldview_exe} {filepath} -SaveSnapshot={filepath}_front.png -DefaultLatLong=90,0")
         # os.system(f"{ldview_exe} {filepath} -SaveSnapshot={filepath}_left.png -DefaultLatLong=0,90")
         os.system(
-            f"{ldview_exe} {filepath} -SaveSnapshot={filepath}_normal.png -DefaultLatLong=30,30"
+            f"{ldview_exe} {filepath} -SaveSnapshot={filepath}.png -DefaultLatLong=40,40 -SaveAlpha=1 -ShowAxes=0 -FOV=12.0 -EdgeThickness=3.0"
         )
 
 
 if __name__ == "__main__":
-    folder_path = r"/Users/apple/workspace/lego-technic-solver/debug/2020-07-21_21-57-35_brick_heads"
+    folder_path = r"/Users/apple/Dropbox/deecamp/parts_library/hair/hair_images/testing"
     render_ldrs(folder_path)
--- solvers/brick_heads/main.py
+++ solvers/brick_heads/main.py
@@ -1,106 +1,97 @@
 from bricks_modeling.file_IO.model_writer import write_bricks_to_file_for_instruction
-from bricks_modeling.connectivity_graph import ConnectivityGraph
 from util.debugger import MyDebugger
 import os
-import itertools as iter
-import json
-import solvers.brick_heads.part_selection as p_select
-import copy
-import numpy as np
 import csv
-from typing import Tuple, List
 import solvers.brick_heads.bach_render_images as render
+import solvers.brick_heads.part_selection as p_select
 
+def string_to_tuple_list(tuple_str):
+    if tuple_str == "[]":
+        return []
+    else:
+        str_list = tuple_str.split("), (")
+        str_list[0] = str_list[0][2:]
+        str_list[-1] = str_list[-1][:-2]
+        result_list = []
+        for t in str_list:
+            numbers = t.split(", ")
+            if len(numbers) == 3:
+                result_list.append((
+                    int(numbers[0]),
+                    int(numbers[1]),
+                    int(numbers[2])
+                ))
+            elif len(numbers) == 2:
+                result_list.append((
+                    int(numbers[0]),
+                    int(numbers[1])
+                ))
 
-def gen_LEGO_figure(
-    gender: int,
-    hair: int,
-    hair_color: Tuple,
-    bang: int,
-    skin_color: int,
-    eye: int,
-    jaw: int,
-    hands: int,
-    clothes_style: int,
-    clothes_bg_color: Tuple,
-    pants_type: int,
-    pants_color: Tuple
-):
-    # Template
-    template_head_bricks, template_bottom_bricks = p_select.gen_template()
+        return result_list
 
-    # Hair
-    hair_bricks , hair_skin_bricks = p_select.gen_hair(gender, hair, hair_color, bang, skin_color)
+def string_to_str_list(tuple_str):
+    if tuple_str == "[]":
+        return []
+    else:
+        str_list = tuple_str.split("', '")
+        str_list[0] = str_list[0][2:]
+        str_list[-1] = str_list[-1][:-2]
 
-    # Eye
-    eye_bricks , eye_skin_bricks = p_select.gen_eyes(eye, skin_color)
-
-    # Jaw
-    jaw_bricks , jaw_skin_bricks = p_select.gen_jaw(jaw, skin_color)
-
-    # Hands
-    hands_bricks , hands_skin_bricks = p_select.gen_hands(hands, skin_color, clothes_bg_color)
-
-    # Clothes
-    clothes_bricks = p_select.gen_clothes(clothes_style, clothes_bg_color)
-
-    # Legs
-    legs_bricks = p_select.gen_leges(pants_type, clothes_bg_color, pants_color, skin_color)
-
-    return (
-        clothes_bricks
-        + hands_bricks + hands_skin_bricks
-        + template_head_bricks
-        + eye_skin_bricks + eye_bricks
-        + jaw_skin_bricks + jaw_bricks
-        + hair_skin_bricks + hair_bricks
-        + legs_bricks
-        + template_bottom_bricks
-    )
-
-
+        return str_list
 
 
 if __name__ == "__main__":
     debugger = MyDebugger("brick_heads")
-    csv_path = r"/Users/apple/Dropbox/deecamp/data.csv"
+
+    csv_path = os.path.join(os.path.dirname(__file__), "input_data", "data.csv")
 
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         csv_matrix = list(csv_reader)
 
-    for model in csv_matrix[2:101]:
+    for model in csv_matrix[54:55]:
         if model[1] == "":
             continue
-        bricks = gen_LEGO_figure(
+        bricks, texture_brick_strs, ordered_bricks = p_select.gen_LEGO_figure(
             gender=int(model[1]),
-            hair=int(model[2]),
+            # gender=0,
+            # hair=int(model[2]),
+            hair=5,
             hair_color=(
                 int(model[3].split(",")[0]),
                 int(model[3].split(",")[1]),
                 int(model[3].split(",")[2]),
             ),
-            bang=int(model[4]),
+            # bang=int(model[4]),
+            bang=0,
             skin_color=int(model[5]),
             eye=int(model[6]),
             jaw=int(model[7]),
             hands=int(model[8]),
             clothes_style=int(model[9]),
-            clothes_bg_color = (
-                int(model[10].split(", ")[0][3:]),
-                int(model[10].split(", ")[1]),
-                int(model[10].split(", ")[2][:model[10].split(", ")[2].index("]")])
-            ),
+            # clothes_bg_color = string_to_tuple_list(model[10]),
+            clothes_bg_color=[],
+            logo_imgs =string_to_str_list(model[11]),
+            logo_pos =string_to_tuple_list(model[12]),
             pants_type = int(model[13]),
-            pants_color = (
-                int(model[14].split(", ")[0][3:]),
-                int(model[14].split(", ")[1]),
-                int(model[14].split(", ")[2][:model[14].split(", ")[2].index("]")])
-            ) if len(model[14].split(", ")) >=3 else None
+            # pants_color = string_to_tuple_list(model[14]),
+            pants_color=[],
         )
 
+        ### hack color for LKF
+        # for b_list in ordered_bricks:
+        #     for b in b_list:
+        #         if b.template.id == "22885":
+        #             b.color = 7
+        #         if b.template.id == "3022":
+        #             b.color = 0
+        #         if b.template.id == "3941":
+        #             b.color = 72
+
         write_bricks_to_file_for_instruction(
-            bricks,
-            file_path=debugger.file_path(f"complete_{int(model[0])}.ldr")
+            ordered_bricks,
+            file_path=debugger.file_path(f"complete_{int(model[0])}.ldr"),
+            texture_brick_strs=texture_brick_strs
         )
-    render.render_ldrs(debugger._debug_dir_name)
+
+    # render.render_ldrs(debugger._debug_dir_name)
