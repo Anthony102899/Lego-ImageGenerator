fix critical bug: losing DoF at some rotation axis

solvers/rigidity_solver/constraint_3d.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 from util.geometry_util import normalize, get_perpendicular_vecs
 from solvers.rigidity_solver.algo_core import project_matrix_3D
 from scipy.linalg import null_space
@@ -27,8 +28,8 @@ def projection_matrix(source_points, target_point):
     norm_1 = np.linalg.norm(x1 - x0)
     basis_1 = (x1 - x0) / norm_1
 
-    norm_2 = np.linalg.norm(np.cross(basis_1, x2 - x0))
-    basis_2 = np.cross(basis_1, x2 - x0) / norm_2
+    norm_2 = np.linalg.norm(np.cross(x2 - x0, basis_1))
+    basis_2 = np.cross(x2 - x0, basis_1) / norm_2
 
     basis_3 = np.cross(basis_1, basis_2)
 
@@ -118,16 +119,20 @@ def prohibitive_space_of_allowed_relative_rotation(
     :param target_point: (3, )
     :param pivot_point: (3, )
     :param rotation_axis: (3, )
-    :return: (2, 3)
+    :return: (m, 3)
     """
     allowed_direction = np.cross(target_point - pivot_point, rotation_axis)
     if np.abs(np.linalg.norm(allowed_direction)) > 1e-8:
         allowed_direction = normalize(allowed_direction)
-        null_basis = null_space(np.array([allowed_direction, allowed_direction])).transpose()
+        null_basis = np.vstack(get_perpendicular_vecs(allowed_direction))
         return null_basis
     else:
-        allowed_direction = np.zeros((3, ))
-        null_basis = null_space(np.array([allowed_direction, allowed_direction])).transpose()
+        sqrt_half = np.sqrt(0.5)
+        null_basis = np.array([
+            [1, 0, 0],
+            [0, sqrt_half, sqrt_half],
+            [0, sqrt_half, -sqrt_half],
+        ])
         return null_basis
 
 
@@ -153,6 +158,9 @@ def constraints_for_allowed_motions(
             relative_rotation_axis = source_transform @ rotation_axis
 
             allowed_motion = np.cross(relative_target_point - relative_rotation_pivot, relative_rotation_axis)
+            allowed_motion /= np.linalg.norm(allowed_motion)
+
+            print(relative_projection)
 
             prohibitive_space = prohibitive_space_of_allowed_relative_rotation(
                 relative_target_point,

solvers/rigidity_solver/gradient.py
@@ -10,16 +10,15 @@ Gradient analysis:
     - joint orientation
 """
 
-
 def gradient_analysis(
         points: np.ndarray,
         edges: np.ndarray,
         hinge_axes,
         hinge_pivots,
         hinge_point_indices,
-        dim=3
-) -> np.ndarray:
-    torch.autograd.set_detect_anomaly(True)
+        extra_constraints=None,
+        iters=10000
+) -> torch.Tensor:
 
     points = torch.tensor(points, dtype=torch.double, requires_grad=True)
     edges = torch.tensor(edges)
@@ -27,27 +26,39 @@ def gradient_analysis(
     hinge_pivots = torch.tensor(hinge_pivots, dtype=torch.double, requires_grad=True)
     hinge_point_indices = torch.tensor(hinge_point_indices, dtype=torch.long)
 
-    energy = spring_energy_matrix(points, edges, dim)
-    constraint = constraint_matrix(points, hinge_axes, hinge_pivots, hinge_point_indices)
+    optimizer = torch.optim.Adam([hinge_axes], lr=1e-2)
 
-    # trivial_basis = torch.from_numpy(geo_util.trivial_basis(points.detach().cpu().numpy(), 3))
-    # constraint = torch.vstack((constraint, trivial_basis))
+    from tqdm import tqdm
+    for i in tqdm(range(iters)):
+        optimizer.zero_grad()
 
-    B = torch_null_space(constraint)
-    print(B.size())
-    T = torch.mm(B.t(), B)
-    S = torch.chain_matmul(B.t(), energy, B)
-    L = torch.cholesky(T)
-    L_inv = torch.inverse(L)
+        energy = spring_energy_matrix(points, edges, 3)
+        constraint = constraint_matrix(points, hinge_axes, hinge_pivots, hinge_point_indices)
 
-    Q = torch.chain_matmul(L_inv.t(), S, L_inv)
+        if extra_constraints is not None:
+            constraint = torch.vstack([constraint, torch.from_numpy(extra_constraints).double()])
 
-    eigenvalues, eigenvectors = torch.symeig(Q, eigenvectors=True)
+        B = torch_null_space(constraint)
+        T = torch.mm(B.t(), B)
+        S = torch.chain_matmul(B.t(), energy, B)
+        L = torch.cholesky(T)
+        L_inv = torch.inverse(L)
 
-    print(eigenvalues)
-    least_eigenvalue = torch.min(eigenvalues)
-    least_eigenvalue.backward()
-    print(hinge_axes.grad)
+        Q = torch.chain_matmul(L_inv.t(), S, L_inv)
+
+        eigenvalues, eigenvectors = torch.symeig(Q, eigenvectors=True)
+
+        obj = torch.min(eigenvalues)
+
+        # if i % 20 == 0:
+        #     print("Objective", obj.detach().numpy())
+        #     print("Axes", hinge_axes)
+
+        obj.backward()
+        optimizer.step()
+
+    return obj
+
 
 
 def rigidity_matrix(
@@ -138,7 +149,6 @@ def perpendicular_vectors(v: torch.Tensor):
 def torch_null_space(A: torch.Tensor):
     with torch.no_grad():
         g, d, h = torch.svd(A, some=False)
-        print(g.size(), d.size(), h.size())
         eps = torch.linspace(1e-6, 1e-4, len(d))
         eps_diag = torch.zeros_like(A)
         eps_diag[:len(d), :len(d)] = torch.diag_embed(eps)
@@ -153,8 +163,6 @@ def torch_null_space(A: torch.Tensor):
     M, N = u.size()[0], vt.size()[1]
     rcond = torch.finfo(s.dtype).eps * max(M, N) + 1e-4
     tol = torch.max(s) * rcond
-    print(s)
-    print(tol)
     num = torch.sum(s > tol, dtype=torch.int)
 
     Q = vt[num:, :].t().conj()

solvers/rigidity_solver/internal_structure.py
@@ -67,7 +67,9 @@ def tetrahedronize(p, q, thickness=1):
     rot = find_rotation_matrix(z, n)
 
     base = (rot @ vertices.T).T
-    points = np.vstack(np.linspace(base + p, base + q, num=5))
+    start = base + (p + 0.01 * (q - p))
+    end = base + (q + 0.01 * (p - q))
+    points = np.vstack(np.linspace(start, end, num=5))
     edges = np.array(
         [[3 * i + 0, 3 * i + 1] for i in range(5)] +
         [[3 * i + 1, 3 * i + 2] for i in range(5)] +

solvers/rigidity_solver/joints.py
@@ -23,7 +23,7 @@ class Model:
         edge_indices = []
         index_offset = 0
         for beam in self.beams:
-            edge_indices.append(beam.edges() + index_offset)
+            edge_indices.append(beam.edges + index_offset)
             index_offset += beam.point_count
         # for joint in self.joints:
         #     edge_indices.append(joint.edges() + index_offset)
@@ -43,7 +43,7 @@ class Model:
 
     @property
     def point_count(self):
-        return sum(beam.point_count for beam in self.beams) + sum(joint.virtual_point_count for joint in self.joints)
+        return sum(beam.point_count for beam in self.beams)
 
     def add_beam(self, beam):
         self.beams.append(beam)
@@ -80,7 +80,7 @@ class Model:
 class Beam:
     def __init__(self, points, edges=None):
         if edges is None:
-            index_range = range(len(self.points))
+            index_range = range(len(points))
             edges = np.array(list(itertools.combinations(index_range, 2)))
 
         self._edges = edges

tests/dev_sympy.py
@@ -107,11 +107,12 @@ varlist = [
     norm_1, norm_2,
 ] + deltas
 
+#%%
 constant_values = np.array([
-    0, 0, 0,
-    1, 0, 0,
     0, 1, 0,
-    0, 0, 1,
+    0, 1, 1,
+    0, 2, 0,
+    1, 0, 1,
 ])
 x1_minus_x0 = np.array(constant_values[3: 6]) - np.array(constant_values[0: 3])
 v1 = x1_minus_x0
@@ -124,13 +125,13 @@ norm_values = [
 ]
 delta_values = np.array([
     0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-]) * 1e-9
+    -1, 1, 1,
+    0, 0, 2,
+    1, -1, 1,
+]) 
 
 context = {var: value for var, value in zip(varlist, np.hstack((constant_values, norm_values, delta_values)))}
-dictsubs(T_prime, context)
+dictsubs(delta_b, context)
 
 #%%
 
@@ -147,11 +148,10 @@ for i, j in tqdm(product(range(3), range(12))):
 
     projection_matrix[i, j] = sympy.simplify(order_one_coeff)
 
-# projection_offset = zeros(3, 1)
-# for i in range(3):
-#     coeff = expanded_delta_b[i]
-#     order_zero_coeff = (coeff + O_deltas).removeO()
-#     projection_offset[i, 0] = order_zero_coeff
+dictsubs(projection_matrix, context)
+#%%
+dictsubs(projection_matrix @ Matrix(deltas), context)
+
 #%%
 
 # for var_index in [3, 4, 5]: